d94cf4e46ff568cdce70048678845d992c7ca8ff
Pablo Joel <pablojoel56@hotmail.com>
2022-07-31 13:20:29 +0200

    Grammarian is now a Class

--- Grammarian.py
+++ Grammarian.py
@@ -12,7 +12,6 @@ def get_config(path):
 
     return config
 
-
 def create_spell(tokens, new_token, token_index):
     spell = list()
     spell.extend(tokens[:token_index])
@@ -22,28 +21,32 @@ def create_spell(tokens, new_token, token_index):
     res = " "
     return res.join(spell)
 
-def grammarian(spell, language_file, language_alphabet):
-    tokens = spell.split(' ')   # Split the spell into tokens (words)
-    new_spells = list()
 
-    for token_index, token in enumerate(tokens):
-        new_tokens = list()
-        for char_index in range(len(token)):
-            new_tokens = [token[:char_index] + letter + token[char_index+1:] for letter in language_alphabet if token[char_index] != letter]   # Generate new words by modifying the token
-            new_tokens = [create_spell(tokens, new_token, token_index) for new_token in new_tokens]             # Append new words to the other tokens
-            new_spells.extend(new_tokens)
-    return new_spells
+class Grammarian:
+    def __init__(self, config_path):
+        config = get_config(config_path)
+        language_path = config.get('LANGUAGE CONFIG', 'lang_config_path')
+        language_config = get_config(path=language_path)
+
+        self.language_file = language_config.get('SOURCE', 'source_file')
+        self.language_alphabet = language_config.get('ALPHABET', 'alphabet').split(',')
+
+    def execute(self, spell):
+        tokens = spell.split(' ')   # Split the spell into tokens (words)
+        new_spells = list()
+
+        for token_index, token in enumerate(tokens):
+            for char_index in range(len(token)):
+                new_tokens = [token[:char_index] + letter + token[char_index+1:] for letter in self.language_alphabet if token[char_index] != letter]   # Generate new words by modifying the token
+                new_tokens = [create_spell(tokens, new_token, token_index) for new_token in new_tokens]             # Append new words to the other tokens
+                new_spells.extend(new_tokens)
+        return new_spells
 
 
 if __name__ == '__main__':
-    grammarian_config = get_config(path='grammarian_config.ini')
-    language_path = grammarian_config.get('LANGUAGE CONFIG', 'lang_config_path')
+    grammarian = Grammarian(config_path='grammarian_config.ini')
 
-    language_config = get_config(path=language_path)
-    language_file = language_config.get('SOURCE', 'source_file')
-    language_alphabet = language_config.get('ALPHABET', 'alphabet').split(',')
-
-    spells = grammarian(spell='mage hand', language_file=language_file, language_alphabet=language_alphabet)
+    spells = grammarian.execute(spell='mage hand')
     print(spells)
 
 
