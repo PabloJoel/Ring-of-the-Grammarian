059282a7986352ed89918d7d004778a76f628525
Pablo Joel <pablojoel56@hotmail.com>
2022-08-10 18:44:31 +0200

    Configuration path is no longer needed

--- Utils/ReadConfig.py
+++ Utils/ReadConfig.py
@@ -1,15 +1,27 @@
 from configparser import ConfigParser
 
+from pathlib import Path
+import os.path
 
-def get_config(config_path):
+
+def get_root_project_path():
+    """
+    Returns the root path of the project
+
+    :return str: project root path
+    """
+    return str(Path(__file__).parent.parent)
+
+
+def get_config():
     """
     Read ini file and return it as a dictionary. The dictionary contains as many keys as there are headers
     in the ini file. The values are dictionaries containing that header information.
 
-    :param str config_path: path to the ini file
     :return dict: configuration
     """
-    parser = ConfigParser()
-    parser.read(config_path)
-    return {section: dict(parser.items(section)) for section in parser.sections()}
+    root_project_path = os.path.join(get_root_project_path(), 'grammarian_config.ini')
 
+    parser = ConfigParser()
+    parser.read(root_project_path)
+    return {section: dict(parser.items(section)) for section in parser.sections()}
