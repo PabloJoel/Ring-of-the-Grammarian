c9feba81ca6d3129388c519168db2119b5b99493
Pablo Joel <pablojoel56@hotmail.com>
2022-08-22 19:45:28 +0200

    Missing word utility improved with tokenization

--- Utils/MissingWords.py
+++ Utils/MissingWords.py
@@ -1,5 +1,7 @@
 import enchant
+from nltk.tokenize import wordpunct_tokenize
 
+from Grammarian import Grammarian
 from ReadConfig import get_config
 
 
@@ -10,8 +12,15 @@ def check_missing_words(spells_path):
     :param str spells_path: path to a file containing text
     :return set: set containing words that are not registered in the dictionary
     """
+    gm = Grammarian()
+
+    tokens = list()
     with open(spells_path, 'r') as spells_file:
-        spells = spells_file.read().split()
+        spells = spells_file.read().split('\n')
+        for spell in spells:
+            tokens.extend(wordpunct_tokenize(str.lower(spell)))
+
+    spells = [token for token in tokens if gm.is_correct_word(token=token, regex=gm.get_alphabet_regex())]
 
     config = get_config()
     # Create dictionary
