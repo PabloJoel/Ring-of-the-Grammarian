a5c417daf693bc1af8bf5ee2519a898a2a818481
Pablo Joel <pablojoel56@hotmail.com>
2022-08-23 12:29:33 +0200

    Add documentation to get_personal_words_path function

--- Utils/FileUtils.py
+++ Utils/FileUtils.py
@@ -27,12 +27,27 @@ def get_config():
 
 
 def get_personal_words_path(language):
+    """
+    This function reads personal words from PersonalWords/all.txt and from PersonalWords/<language>.txt, and combines
+    everything in PersonalWords/generated/<language>_generated.txt
+
+    :param str language: name of the language dictionary
+    :return str: path to PersonalWords/generated/<language>_generated.txt
+    """
+    # PersonalWords folder path
     personal_words_path = os.path.join(get_root_project_path(), 'PersonalWords')
 
+    # PersonalWords\\all.txt file path
     personal_words_all_path = os.path.join(personal_words_path, 'all.txt')
+
+    # PersonalWords\\<language>.txt file path
     personal_words_language_path = os.path.join(personal_words_path, f'{language}.txt')
 
+    # PersonalWords\\generated folder path
     personal_words_path = os.path.join(personal_words_path, 'generated')
+    Path(personal_words_path).mkdir(exist_ok=True)  # Create generated folder if it doesnt exist
+
+    # PersonalWords\\generated\\<language>_generated.txt file path
     personal_words_path = os.path.join(personal_words_path, f'{language}_generated.txt')
 
     with open(personal_words_all_path, "r", encoding='utf-8') as personal_words_all_file:
